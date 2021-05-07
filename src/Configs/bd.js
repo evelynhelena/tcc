2da1b47178af5a5226375d5a42dcd8a7e11f01f4
henriquerick12 <33300487+henriquerick12@users.noreply.github.com>
2021-05-07 18:56:36 -0300

    Config .env

--- src/Configs/bd.js
+++ src/Configs/bd.js
@@ -3,10 +3,10 @@ const mysql = require('mysql')
 // CONFIGURANDO DATABASE
 module.exports = async () => {
         const teste = await mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "",
-            database: "db_mercadinho"
+            host: process.env.MYSQL_HOST,
+            user: process.env.MYSQL_USER,
+            password: process.env.MYSQL_PASS,
+            database: process.env.MYSQL_DB,
         })
         
         return teste
