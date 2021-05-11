0dc19f5373a3ea966acfad4022ebd522dcad3f6b
Evelyn <evelynhelena781@gmail.com>
2021-05-10 21:55:13 -0300

    Arrumando bug da abertura do modal

--- client/src/views/modais/UserInsert.jsx
+++ client/src/views/modais/UserInsert.jsx
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
-import * as bootstrap from "react-bootstrap";
+import { Modal, Button, Form, Row, Col } from "react-bootstrap";
 import * as FaIcons from "react-icons/fa";
 import api from "../../services/Api";
-function ModalInsertUser() {
+
+function ModalInsertUser(props) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
   const [usersType, setUsersType] = useState([]);
-
-
 
   const getUsersType = async () => {
     try {
       const { data } = await api.get("http://localhost:3000/findUserType");
-      if (data)  setUsersType(data);
+      if (data) setUsersType(data);
     } catch (err) {
       console.log(err);
     }
@@ -22,93 +20,75 @@ function ModalInsertUser() {
 
   return (
     <>
-      {!usersType ? (
-        <>
-          <p>Carregando...</p>
-        </>
-      ) : (
-        <>
-        {console.log(usersType)}
-          <bootstrap.Button
-            variant="primary"
-            className="btn-plus edit"
-            onClick={function(){
-                handleShow();
-                getUsersType();
-            }}
-          >
-            <FaIcons.FaPlus />
-          </bootstrap.Button>
-          <bootstrap.Modal show={show} onHide={handleClose} size="lg">
-            <bootstrap.Modal.Header closeButton className="modal-header">
-              <bootstrap.Modal.Title>
-                Inserir novo Usuário
-              </bootstrap.Modal.Title>
-            </bootstrap.Modal.Header>
-            <bootstrap.Modal.Body>
-              <bootstrap.Form>
-                <bootstrap.Row>
-                  <bootstrap.Col xs={6} md={6}>
-                    <bootstrap.Form.Group controlId="name">
-                      <bootstrap.Form.Label>Nome</bootstrap.Form.Label>
-                      <bootstrap.Form.Control type="text" placeholder="Nome" />
-                    </bootstrap.Form.Group>
-                  </bootstrap.Col>
+      <Button
+        variant="primary"
+        className="btn-plus edit"
+        onClick={function () {
+          handleShow();
+          getUsersType();
+        }}
+      >
+        <FaIcons.FaPlus />
+      </Button>
 
-                  <bootstrap.Col xs={6} md={6}>
-                    <bootstrap.Form.Group controlId="lastName">
-                      <bootstrap.Form.Label>Sobrenome</bootstrap.Form.Label>
-                      <bootstrap.Form.Control
-                        type="text"
-                        placeholder="Sobrenome"
-                      />
-                    </bootstrap.Form.Group>
-                  </bootstrap.Col>
-                </bootstrap.Row>
+      <Modal show={show} onHide={handleClose} animation={false} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Inserir novo Usuário</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Row>
+              <Col xs={6} md={6}>
+                <Form.Group controlId="name">
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control type="text" placeholder="Nome" />
+                </Form.Group>
+              </Col>
 
-                <bootstrap.Row>
-                  <bootstrap.Col xs={6} md={6}>
-                    <bootstrap.Form.Group controlId="userName">
-                      <bootstrap.Form.Label>
-                        Nome de Usuário
-                      </bootstrap.Form.Label>
-                      <bootstrap.Form.Control type="text" placeholder="Nome" />
-                    </bootstrap.Form.Group>
-                  </bootstrap.Col>
+              <Col xs={6} md={6}>
+                <Form.Group controlId="lastName">
+                  <Form.Label>Sobrenome</Form.Label>
+                  <Form.Control type="text" placeholder="Sobrenome" />
+                </Form.Group>
+              </Col>
+            </Row>
 
-                  <bootstrap.Col xs={6} md={6}>
-                    <bootstrap.Form.Group controlId="phone">
-                      <bootstrap.Form.Label>Celular</bootstrap.Form.Label>
-                      <bootstrap.Form.Control
-                        type="text"
-                        placeholder="Celular"
-                      />
-                    </bootstrap.Form.Group>
-                  </bootstrap.Col>
-                </bootstrap.Row>
+            <Row>
+              <Col xs={6} md={6}>
+                <Form.Group controlId="userName">
+                  <Form.Label>Nome de Usuário</Form.Label>
+                  <Form.Control type="text" placeholder="Nome" />
+                </Form.Group>
+              </Col>
 
-                <bootstrap.Form.Group controlId="example.Form.ControlSelect1">
-                  <bootstrap.Form.Label>Tipo de Usuário</bootstrap.Form.Label>
-                  <bootstrap.Form.Control as="select">
-                  <option>Selecionar</option>
-                  {usersType.map((userType) => (
-                    <option kwy={userType.id}>{userType.type_user}</option>
-                  ))}
-                  </bootstrap.Form.Control>
-                </bootstrap.Form.Group>
-              </bootstrap.Form>
-            </bootstrap.Modal.Body>
-            <bootstrap.Modal.Footer>
-              <bootstrap.Button variant="secondary" onClick={handleClose}>
-                Cancelar
-              </bootstrap.Button>
-              <bootstrap.Button variant="info" onClick={handleClose}>
-                Salvar
-              </bootstrap.Button>
-            </bootstrap.Modal.Footer>
-          </bootstrap.Modal>
-        </>
-      )}
+              <Col xs={6} md={6}>
+                <Form.Group controlId="phone">
+                  <Form.Label>Celular</Form.Label>
+                  <Form.Control type="text" placeholder="Celular" />
+                </Form.Group>
+              </Col>
+            </Row>
+
+            <Form.Group controlId="example.Form.ControlSelect1">
+              <Form.Label>Tipo de Usuário</Form.Label>
+              <Form.Control as="select">
+                <option>Selecionar</option>
+                {usersType.map((userType) => (
+                  <option key={userType.id}>{userType.type_user}</option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancelar
+          </Button>
+          <Button variant="info" onClick={handleClose}>
+            Salvar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
